8f334c9d280addc1068dcd24a248454fe59d9240
Nicolas Lacroix <nicolas.lacroix@univ-cotedazur.fr>
2025-11-17 14:27:27 +0100

    feat(js): Add canopus-dsl javascript library

--- ANTLR/compile.sh
+++ ANTLR/compile.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 uv run antlr4 -Dlanguage=Python3 -o ../src/canopus_dsl/generated ./CanopusDSL.g4
+
+uv run antlr4 -Dlanguage=JavaScript -o ../js/canopus-dsl/generated ./CanopusDSL.g4
---
+++ js/canopus-dsl/index.js
@@ -0,0 +1,6 @@
+const lexer = require('./generated/CanopusDSLLexer.js');
+const listener = require('./generated/CanopusDSLListener.js');
+const parser = require('./generated/CanopusDSLParser.js');
+
+
+export { lexer, listener, parser };
